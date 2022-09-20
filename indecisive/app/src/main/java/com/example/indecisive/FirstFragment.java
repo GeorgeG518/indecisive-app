19789fba9259e2a897882c969059f0456cb2140c
George Gannon <gmg3f@mtmail.mtsu.edu>
2022-09-19 19:23:07 -0500

    removed that stupid button

--- indecisive/app/src/main/java/com/example/indecisive/FirstFragment.java
+++ indecisive/app/src/main/java/com/example/indecisive/FirstFragment.java
@@ -28,13 +28,6 @@ public class FirstFragment extends Fragment {
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        binding.diceRoller.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                NavHostFragment.findNavController(FirstFragment.this)
-                        .navigate(R.id.action_FirstFragment_to_SecondFragment);
-            }
-        });
         binding.foodPicker.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view){
