a5ada28e91e0517a623447b23881b05ea1dc3991
sarra zerairia <sz3a@mtmail.mtsu.edu>
2022-11-07 19:05:26 -0600

    more ui stuff

--- indecisive/app/src/main/java/com/example/indecisive/FoodPickerFragment.java
+++ indecisive/app/src/main/java/com/example/indecisive/FoodPickerFragment.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
--- indecisive/app/src/main/java/com/example/indecisive/SecondFragment.java
+++ indecisive/app/src/main/java/com/example/indecisive/SecondFragment.java
@@ -1,40 +1,32 @@
 package com.example.indecisive;
 
 import android.os.Bundle;
-import android.preference.EditTextPreference;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
-
 import com.example.indecisive.databinding.FragmentSecondBinding;
+
+import java.util.Random;
 
 public class SecondFragment extends Fragment {
 
     private FragmentSecondBinding binding;
 
-    int firstNumber;
-    int secondNumber;
-    int numberAmount;
+    Integer num1;
+    Integer num2;
+    Integer num3;
 
-    Button Submit;
+    boolean fieldChecked = false;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-     //   setContentView(R.layout.fragment_second);
+    }
 
-     //  firstNumberInput = (EditText) findViewById(R.id.firstNumberInput);
-      //  secondNumberInput = (EditText) findViewById(R.id.secondNumberInput);
-        //numberAmountInput = (EditText) findViewById(R.id.numberAmountInput);
-
-      //  submitButton = (Button) findViewById(R.id.submitButton);
-        //submitButton.setOnClickListener(new View.OnClickListener()
- /*
     @Override
     public View onCreateView(
             LayoutInflater inflater, ViewGroup container,
@@ -56,6 +48,60 @@ public class SecondFragment extends Fragment {
                         .navigate(R.id.action_SecondFragment_to_FirstFragment);
             }
         });
+
+        binding.numberEnterbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+
+                fieldChecked = CheckValues();
+
+                if(CheckValues())
+                {
+
+                    findNumbers();
+
+                    Random random = new Random();
+                    binding.finalRandomNumbers.setText(" ");
+
+                    for (int i = 0; i < num3; i++) {
+                       int x = random.nextInt(num2 - num1 + 1) + num1;
+                       binding.finalRandomNumbers.append(x + " ");
+                    }
+                }
+            }
+        });
+
+    }
+
+    public void findNumbers() {
+
+        num1 = Integer.parseInt(String.valueOf(binding.NumberOneTextBox.getText()));
+        num2 = Integer.parseInt(String.valueOf(binding.TextNumber2Box.getText()));
+        num3 = Integer.parseInt(String.valueOf(binding.NumberAmountTextBox.getText()));
+    }
+
+
+    private boolean CheckValues()
+    {
+
+        if (!binding.NumberOneTextBox.getText().toString().matches("[0-9]+"))
+        {
+            binding.NumberOneTextBox.setError("This field is required");
+            return false;
+        }
+        if (!binding.TextNumber2Box.getText().toString().matches("[0-9]+"))
+        {
+            binding.TextNumber2Box.setError("This field is required");
+            return false;
+        }
+        if (!binding.NumberAmountTextBox.getText().toString().matches("[0-9]+"))
+        {
+            binding.NumberAmountTextBox.setError("This field is required");
+            return false;
+        }
+        else
+            return true;
     }
 
     @Override
@@ -63,8 +109,5 @@ public class SecondFragment extends Fragment {
         super.onDestroyView();
         binding = null;
     }
-
-*/
-
 
 }
