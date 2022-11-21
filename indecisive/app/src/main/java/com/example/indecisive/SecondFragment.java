87a92c1e1dec117391b7e36c5104d3b63b7d3102
ahmadmohammad <ahmadsmohammad02@gmail.com>
2022-11-21 16:14:46 -0600

    ahmad new user input frag connect to icon and functionality

--- indecisive/app/src/main/java/com/example/indecisive/SecondFragment.java
+++ indecisive/app/src/main/java/com/example/indecisive/SecondFragment.java
@@ -25,7 +25,7 @@ public class SecondFragment extends Fragment {
     Button Submit;
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+        super.onCreate(savedInstanceState);}
      //   setContentView(R.layout.fragment_second);
 
      //  firstNumberInput = (EditText) findViewById(R.id.firstNumberInput);
@@ -34,7 +34,7 @@ public class SecondFragment extends Fragment {
 
       //  submitButton = (Button) findViewById(R.id.submitButton);
         //submitButton.setOnClickListener(new View.OnClickListener()
- /*
+
     @Override
     public View onCreateView(
             LayoutInflater inflater, ViewGroup container,
@@ -64,7 +64,7 @@ public class SecondFragment extends Fragment {
         binding = null;
     }
 
-*/
+
 
 
 }
--- indecisive/app/src/main/java/com/example/indecisive/userinput.java
+++ indecisive/app/src/main/java/com/example/indecisive/userinput.java
@@ -1,12 +1,27 @@
+/*
+* user input randomizer fragment
+* developed by Ahmad Mohammad
+* 11-21
+* */
+
 package com.example.indecisive;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.example.indecisive.databinding.FragmentSecondBinding;
+import com.example.indecisive.databinding.FragmentUserinputBinding;
+
+import java.util.Random;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -14,6 +29,8 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class userinput extends Fragment {
+
+    private FragmentUserinputBinding binding;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -23,6 +40,8 @@ public class userinput extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    private int i = 0;
+    private String[] arr = new String[100];
 
     public userinput() {
         // Required empty public constructor
@@ -59,6 +78,95 @@ public class userinput extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_userinput, container, false);
+
+        binding = FragmentUserinputBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+
+        //return inflater.inflate(R.layout.fragment_userinput, container, false);
+
+
+
     }
+
+       // binding = FragmentSecondBinding.inflate(inflater, container, false);
+        //return binding.getRoot();
+
+
+
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+
+
+        binding.enterinputbutton.setOnClickListener(new View.OnClickListener(){
+
+                @Override
+
+
+                public void onClick(View view) {
+                    // create function to get input field box (binding input names)
+                    // maybe add to array
+
+                     /*
+                     * init empty string to fill with
+                     * "enterinputnames text field"
+                     * */
+
+                    String holder = "";
+
+                    holder = String.valueOf(binding.enterinputnames.getText());
+                    arr[i] = holder; // assign array at i to textvield value
+                    i++;
+
+                    // clear
+
+                    binding.enterinputnames.setHint("Enter next choice!");
+
+                    binding.textView14.setText("items added: " + i);
+
+                }
+            });
+
+
+
+
+
+
+
+        binding.finishedbutton.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+
+
+            public void onClick(View view) {
+                // maybe create function to get input firld box
+                // amd maybe add to array
+
+                Random randnum = new Random(100-1);
+                int q = randnum.nextInt(i-1);
+
+                i = 0;
+
+                binding.winnernametextfield.setText(arr[q]);
+                binding.textView14.setText("items added: " + i);
+
+                for(int p = 0; p < arr.length; p++)
+                {
+                    arr[p] = "";
+                }
+
+
+
+            }
+        }
+
+        );
+
+
+    }
+
+
+
 }
+
